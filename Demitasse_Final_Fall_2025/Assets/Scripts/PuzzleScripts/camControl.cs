1d24d3b5893662678f279ace0305703199c81be4
Scout Golladay <floralgolladay@gmail.com>
2025-11-12 09:45:17 -0500

    Changed Editor Version
    +Camera Movement/StateChange Update

--- Demitasse_Final_Fall_2025/Assets/Scripts/PuzzleScripts/camControl.cs
+++ Demitasse_Final_Fall_2025/Assets/Scripts/PuzzleScripts/camControl.cs
@@ -5,23 +5,49 @@ public class camControl : MonoBehaviour, IGameStateManager
 {
     public CinemachineCamera cam;
     public int priority;
+    public int camID;
 
     public void Start()
     {
         cam = GetComponent<CinemachineCamera>();
     }
 
+    /*public void puzzleCamSwitch(int camID_)
+    {
+    }*/
+
     public void GetState(gameState state)
     {
+        // when the state is _# puzzle
         if (state.state == gameState.STATE.PUZZLE1)
         {
-            // go to puzzle 1
-            cam.Priority = priority;
+            // try switch state thing later
+            /*switch (camID_)
+            {
+                case 1:
+                    priority = 15;
+                    break;
+            }*/
+
+            // depending on the camera's id #
+            if (camID == 1)
+            {
+                // move cam up priority
+                cam.Priority = priority;
+                priority = 15;
+            }
+            // doesn't move cam up priority
+            else
+            {
+                priority = 0;
+            }
         }
+        // on other states
         else if (state.state == gameState.STATE.MENU)
         {
             // go to menu
             cam.Priority = 0;
         }
     }
+
 }
