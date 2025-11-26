ac9defbdfa03e65a307a710ac4304d4b0f0a31cd
pheapchin <pheapxchin@gmail.com>
2025-11-26 12:31:36 -0500

    cleaner dialog transition
    end dialog now on tab button

--- Demitasse_Final_Fall_2025/Assets/Scripts/GameManagerScripts/GameManager.cs
+++ Demitasse_Final_Fall_2025/Assets/Scripts/GameManagerScripts/GameManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject letterGroup;
-    
+    public UnityEvent dialogTrigger;
+
     public void Update()
     {
         // temporary key to move to the puzzle scene
@@ -18,6 +20,11 @@ public class GameManager : MonoBehaviour
         if (Keyboard.current[Key.Escape].wasPressedThisFrame)
         {
             letterGroup.SetActive(false);
+        }
+
+        if (Keyboard.current[Key.Escape].wasPressedThisFrame)
+        {
+            dialogTrigger.Invoke();
         }
     }
 
